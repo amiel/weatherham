fd7935ff7d9942997c403bf457aedad682e88074
Amiel Martin <amiel.martin@gmail.com>
2009-11-06 00:29:18 -0800

    re-order

--- app/controllers/observations_controller.rb
+++ app/controllers/observations_controller.rb
@@ -1,5 +1,5 @@
 class ObservationsController < ApplicationController
 	def index
-		@observations = Observation.paginate :page => params[:page], :per_page => 100
+		@observations = Observation.paginate :page => params[:page], :per_page => 10, :order => 'observed_at DESC'
 	end
 end
