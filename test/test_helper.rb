d94701763a0093bcc33d715f96dd15fd1fdf38b7
Amiel Martin <amiel.martin@gmail.com>
2009-11-05 23:49:15 -0800

    setup test libraries
    * require authlogic test helpers in test_helper
    * require shoulda and mocha in test_helper
    * add gems to test.rb
    * generate cucumber

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,9 @@
 ENV["RAILS_ENV"] = "test"
 require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
 require 'test_help'
+require 'shoulda'
+require 'mocha'
+require 'authlogic/test_case'
 
 class ActiveSupport::TestCase
   # Transactional fixtures accelerate your tests by wrapping each test method
