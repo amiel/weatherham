e1ad6623f7672ffbc290fcb1b50431ff2de2e809
Amiel Martin <amiel.martin@gmail.com>
2009-11-06 01:10:15 -0800

    slightly nicer observations

--- app/controllers/observations_controller.rb
+++ app/controllers/observations_controller.rb
@@ -1,5 +1,5 @@
 class ObservationsController < ApplicationController
 	def index
-		@observations = Observation.paginate :page => params[:page], :per_page => 10, :order => 'observed_at DESC'
+		@observations = Observation.paginate :page => params[:page], :per_page => 10, :order => 'id DESC'
 	end
 end
