743b2d4a60adafa10714dfba02fd47af01e15784
Amiel Martin <amiel@carnesmedia.com>
2012-02-25 21:52:15 -0800

    The easy way out

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,8 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.11' unless defined? RAILS_GEM_VERSION
-# RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
+# RAILS_GEM_VERSION = '2.3.11' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 # https://makandracards.com/makandra/1109-fixing-undefined-local-variable-or-method-version_requirements-for-rails-gemdependency-0x-nameerror
 # or http://excid3.com/blog/undefined-local-variable-or-method-version_requirements-for-nameerror/
