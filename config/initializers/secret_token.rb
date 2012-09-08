670801e48b68926dacc74c26432a9a5aa788e700
Amiel Martin <amiel@carnesmedia.com>
2012-09-08 11:37:59 -0700

    Generated new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Weatherham::Application.config.secret_token = 'f5cccd0b3f2f0ed16ad158b08c1d0d0b8ee3d121ea9ae73d022e71d057e108c9989411e495a1e26d63bc9e07c0afb1a2785a745e223535aa30c8f54909a0d8bd'
