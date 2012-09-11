aa75354cc33c63c991d90502a88fc40615d19ac6
Amiel Martin <amiel@carnesmedia.com>
2012-09-10 21:35:29 -0700

    TODO, changelog, about, fancyness

--- Gemfile
+++ Gemfile
@@ -24,6 +24,7 @@ gem 'jquery-rails'
 gem 'thin'
 gem 'pg'
 gem 'draper'
+gem 'RedCloth'
 
 group :development do
   gem 'pry-rails'
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,20 @@
 class PagesController < ApplicationController
+
+  def changelog
+    @changelog ||= begin
+      lines = Rails.root.join('CHANGELOG.textile').read
+      RedCloth.new(lines.to_s).to_html
+    end
+
+    fresh_when(etag: Digest::SHA1.hexdigest(@changelog), public: true)
+  end
+
+  def todo
+    @todo ||= begin
+      lines = Rails.root.join('TODO.textile').read
+      RedCloth.new(lines.to_s).to_html
+    end
+
+    fresh_when(etag: Digest::SHA1.hexdigest(@todo), public: true)
+  end
 end
