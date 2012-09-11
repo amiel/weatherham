7b2f6d95d98b2d9e6f33f6bde95db98512711d35
Amiel Martin <amiel@carnesmedia.com>
2012-09-10 21:25:54 -0700

    The last controllers we actually need

--- app/controllers/observations_controller.rb
+++ app/controllers/observations_controller.rb
@@ -14,4 +14,45 @@ class ObservationsController < ApplicationController
     @barometer_direction = Observation.current_barometer_direction if @last_observation
     @granularities = GRANULARITIES.keys
   end
+
+
+  def show
+    if Observation.need_fetch? and Rails.env.production? then
+      Fetch.start!
+    end
+
+    klass = GRANULARITIES[params[:id]]
+    if stale?(fresh_options(klass.last)) then
+
+      @ranges = {
+        barometer: {
+          max: klass.maximum(:barometer),
+          min: klass.minimum(:barometer)
+        },
+        max: %w( hi_speed temp humidity ).collect{|a| klass.maximum a }.max
+      }
+
+      n = klass.zoom / klass.period
+      @observations = klass.limit(n).offset([klass.count - n, 0].max).all
+    end
+  end
+
+  def range
+    klass = GRANULARITIES[params[:granularity]]
+    range = Time.from_json(params[:range_begin]) .. Time.from_json(params[:range_end])
+    if stale?(etag: Digest::SHA1.hexdigest("#{klass}/#{range}")) then
+      @observations = klass.where(observed_at: range).all
+      render 'show'
+    end
+  end
+
+  def statistics
+    # {"observation":{"wind_dir":"SSE","temp":54.2,"low_temp":null,"hi_speed":27.0,"observed_at":"2010-04-27T11:10:00-07:00","humidity":71,"created_at":"2010-04-27T11:14:47-07:00","barometer":29.378,"updated_at":"2010-04-27T11:14:47-07:00","wind_chill":47.6,"wind_speed":16.0,"id":33531,"hi_dir":"SSE","wind_run":1.33,"hi_temp":null,"dew_point":45.0}}
+    @statistics = Observation.statistics
+    respond_to do |with|
+      with.xml { render xml: @statistics }
+      with.json { render json: @statistics }
+    end
+  end
+
 end
